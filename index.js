7f291c96eb35aecf54477443ac5813eec218af62
Unknown User <jumperkylian@outlook.com>
2023-01-10 18:27:33 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const { token, options } = require('./config/config.json');
-
-const manager = new ShardingManager('./SRC/bot.js', options);
-
-manager.spawn();
-manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
